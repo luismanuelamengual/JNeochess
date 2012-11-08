d148b4e328c384df141a5816ca522da91baf0e92
luis <luis@luis>
2012-11-08 09:56:42 -0300

    Subo implementación de funciones de búsqueda de movimientos al ComputerPlayer

--- sources/trunk/org/neochess/engine/ComputerPlayer.java
+++ sources/trunk/org/neochess/engine/ComputerPlayer.java
@@ -1,7 +1,92 @@
 
 package org.neochess.engine;
 
+import org.neochess.engine.openingbooks.OpeningBook;
+import org.neochess.engine.searchagents.SearchAgent;
+
 public class ComputerPlayer extends Player
 {
+    private OpeningBook openingBook;
+    private SearchAgent searchAgent;
+
+    public ComputerPlayer (SearchAgent searchAgent)
+    {
+        this(searchAgent, null);
+    }
+
+    public ComputerPlayer (SearchAgent searchAgent, OpeningBook openingBook)
+    {
+        this.openingBook = openingBook;
+        this.searchAgent = searchAgent;
+    }
+
+    @Override
+    public void dispose ()
+    {
+        if (openingBook != null)
+        {
+            openingBook.dispose();
+            openingBook = null;
+        }
+        if (searchAgent != null)
+        {
+            searchAgent.dispose();
+            searchAgent = null;
+        }
+        super.dispose();
+    }
     
+    public synchronized Move startMoveSearch (Match match)
+    {
+        Move bestMove = null;
+        Board board = match.getBoard();
+        int movesMade = match.getPly() / 2;
+        
+        if (openingBook != null && movesMade <= 10)
+            bestMove = openingBook.getMove(board);
+        
+        if (bestMove == null)
+        {
+            Clock clock = match.getTurnClock();
+            long searchTime = 30000;
+            if (clock != null)
+            {
+                long remainingTime = clock.getRemainingTime();
+                if (remainingTime > 60000)
+                {
+                    remainingTime = (85 * remainingTime / 100);
+                    if (movesMade < 30)
+                    {
+                        int movesLimit = 30 - movesMade;
+                        searchTime = remainingTime / movesLimit;
+                    }
+                    else
+                    {
+                        searchTime = remainingTime / 10;
+                    }
+                }
+                else
+                {
+                    searchTime = remainingTime / 20;
+                }
+                
+                Clock oppositeClock = match.getClock(Board.getOppositeSide(match.getSideToMove()));
+                if (oppositeClock != null)
+                    searchTime += 20 * (remainingTime - oppositeClock.getRemainingTime()) / 100;
+                if (clock.getIncrement() > 0 && clock.getIncrement() < (remainingTime-searchTime))
+                    searchTime += clock.getIncrement();
+                
+                if (searchTime < 50)
+                    searchTime = 50;
+                searchTime = (95 * searchTime) / 100;
+            }
+            bestMove = searchAgent.startSearch(board, searchTime);
+        }
+        return bestMove;
+    }
+    
+    public void stopMoveSearch ()
+    {
+        searchAgent.stopSearch();
+    }
 }
