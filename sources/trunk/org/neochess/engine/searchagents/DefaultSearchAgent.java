7b8c6c173df189ec4203fa7dfe72e04196cf1dc9
luis <luis@luis>
2012-12-05 16:58:28 -0300

    Inicio de agregado de funcion SEE

--- sources/trunk/org/neochess/engine/searchagents/DefaultSearchAgent.java
+++ sources/trunk/org/neochess/engine/searchagents/DefaultSearchAgent.java
@@ -10,6 +10,7 @@ import org.neochess.engine.Board;
 import org.neochess.engine.Move;
 import org.neochess.engine.evaluators.DefaultEvaluator;
 import org.neochess.engine.evaluators.Evaluator;
+import org.neochess.util.BoardUtils;
 
 public class DefaultSearchAgent extends SearchAgent
 {
@@ -362,4 +363,135 @@ public class DefaultSearchAgent extends SearchAgent
     {
         Collections.sort(moves, Collections.reverseOrder());
     }
+    
+//    private int SEE (Move move)
+//    {
+//        byte f, t, sq, piece, side, xside;
+//        int n, lastval;
+//        int[] swaplist = new int[20];
+//        long b, c, r;
+//        long[] d, e;
+//
+//        f = move.getInitialSquare();
+//        t = move.getEndSquare();
+//        side = searchBoard.getSquareSide(f);
+//        xside = searchBoard.getOppositeSide(side);
+//
+//        /*  Squares attacking t for side and xside  */
+//        b = searchBoard.getSquareAttackers(t, side);
+//        c = searchBoard.getSquareAttackers(t, xside);
+//        b &= BoardUtils.squareBitX[f];
+//        
+//        if (BoardUtils.sliderX[searchBoard.getSquareFigure(f)] == 1)
+//        {
+//            AddXrayPiece(t, f, side,  & b,  & c);
+//        }
+//
+//        d = board.b[side];
+//        e = board.b[xside];
+//        if (move & PROMOTION)
+//        {
+//            swaplist[0] = Value[PROMOTEPIECE(move)] - ValueP;
+//            lastval = -Value[PROMOTEPIECE(move)];
+//        }
+//        else
+//        {
+//            swaplist[0] = (move & ENPASSANT ? ValueP : Value[cboard[t]]);
+//            lastval = -Value[cboard[f]];
+//        }
+//        n = 1;
+//        while (1)
+//        {
+//            if (c == NULLBITBOARD)
+//            {
+//                break;
+//            }
+//            for (piece = pawn; piece <= king; piece++)
+//            {
+//                r = c & e[piece];
+//                if (r)
+//                {
+//                    sq = leadz(r);
+//                    CLEARBIT(c, sq);
+//                    if (xray[piece])
+//                    {
+//                        AddXrayPiece(t, sq, xside,  & c,  & b);
+//                    }
+//                    swaplist[n] = swaplist[n - 1] + lastval;
+//                    n++;
+//                    lastval = Value[piece];
+//                    break;
+//                }
+//            }
+//
+//            if (b == NULLBITBOARD)
+//            {
+//                break;
+//            }
+//            for (piece = pawn; piece <= king; piece++)
+//            {
+//                r = b & d[piece];
+//                if (r)
+//                {
+//                    sq = leadz(r);
+//                    CLEARBIT(b, sq);
+//                    if (xray[piece])
+//                    {
+//                        AddXrayPiece(t, sq, side,  & b,  & c);
+//                    }
+//                    swaplist[n] = swaplist[n - 1] + lastval;
+//                    n++;
+//                    lastval = -Value[piece];
+//                    break;
+//                }
+//            }
+//        }
+//
+//        --n;
+//        while (n)
+//        {
+//            if (n & 1)
+//            {
+//                if (swaplist[n] <= swaplist[n - 1])
+//                {
+//                    swaplist[n - 1] = swaplist[n];
+//                }
+//            }
+//            else
+//            {
+//                if (swaplist[n] >= swaplist[n - 1])
+//                {
+//                    swaplist[n - 1] = swaplist[n];
+//                }
+//            }
+//            --n;
+//        }
+//        return (swaplist[0]);
+//    }
+//    
+//    protected void addXrayPiece (int t, int sq, int side, long b, long c)
+//    {
+//        int dir, nsq, piece;
+//        long a;
+//
+//        dir = BoardUtils.directions[t][sq];
+//        a = BoardUtils.ray[sq][dir] & searchBoard.getBlocker();
+//        if (a == 0)
+//            return;
+//        
+//        nsq = (t < sq ? leadz(a) : trailz(a));
+//        piece = cboard[nsq];
+//        if ((piece == queen) || (piece == rook && dir > 3) || (piece == bishop && dir < 4))
+//        {
+//            if (BitPosArray[nsq] & board.friends[side])
+//            {
+//                 * b |= BitPosArray[nsq];
+//            }
+//            else
+//            {
+//                 * c |= BitPosArray[nsq];
+//            }
+//        }
+//        return;
+//    }
 }
